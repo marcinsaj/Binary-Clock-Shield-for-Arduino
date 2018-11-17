ec8157051a2a9847dfbdf10fe70547007aa3cbcd
Marcin Saj <31963121+marcinsaj@users.noreply.github.com>
2018-11-17 19:40:18 +0100

    Update 11-BinaryClock-12H-RTCInterruptAlarmButtons.ino

--- example/11-BinaryClockRTCInterruptAlarmButtons/11-BinaryClock-12H-RTCInterruptAlarmButtons.ino
+++ example/11-BinaryClockRTCInterruptAlarmButtons/11-BinaryClock-12H-RTCInterruptAlarmButtons.ino
@@ -1,1 +1,50 @@
-
+// Binary Clock Shield for Arduino by Marcin Saj https://nixietester.com
+// https://github.com/marcinsaj/Binary-Clock-Shield-for-Arduino
+//
+// Binary Clock RTC 12H AM/PM with Interrupt, Alarm and Buttons Example
+// This example demonstrates complete Binary Clock with Time and Alarm settings
+// 
+// Here you can find 24H Binary Clock: http://bit.ly/BinaryClock24H
+//
+// The buttons allow you to set the time and alarm - exact hour, minute, second/alarm status.
+// Alarm causes melody to play.  
+// How to use piezo with the tone() command to generate notes you can find here:
+// http://www.arduino.cc/en/Tutorial/Tone
+// PIEZO Datasheet: http://bit.ly/PIEZO-KLJ1230
+//
+// A falling edge at the RTC INT/SQW output causes an interrupt, 
+// which is uses for regular - 1 per second - reading time from RTC and 
+// checking alarm status flag 'A2F'. Since we use RTC INT/SQW output for
+// regular reading current time - square wave output SQW option, 
+// global interrupt flag INTCN is set to 0, this disables the interrupts from both RTC alarms.
+// Referring to the documentation: when the INTCN is set to logic 0, the 'A2F' bit does not initiate an interrupt signal. 
+// By turning off the interrupts from the alarms, we can use the interrupt flag 'A2IE' as an info flag 
+// whether the alarm has been activated or not. 
+// Check RTC datasheet page 11-13 http://bit.ly/DS3231-RTC
+//
+// Hardware:
+// Arduino Uno, Binary Clock Shield for Arduino
+// INT/SQW connected to Arduino pin 3 / INT1
+// PIEZO connected to Arduino pin 11 PWM
+// S3 button connected to Arduino pin A0
+// S2 button connected to Arduino pin A1
+// S1 button connected to Arduino pin A2
+// LEDs      connected to Arduino pin A3
+// RTC SDA   connected to Arduino pin A4
+// RTC SCL   connected to Arduino pin A5
+//
+//                        +------+       +------+       +------+       +------+       +------+
+//                        |LED 16|---<---|LED 15|---<---|LED 14|---<---|LED 13|---<---|LED 12|--<-+
+//                        +------+       +------+       +------+       +------+       +------+    |
+//                                                                                                |
+//    +--------------->-------------->-------------->-------------->-------------->---------------+
+//    |
+//    |    +------+       +------+       +------+       +------+       +------+       +------+
+//    +----|LED 11|---<---|LED 10|---<---|LED 09|---<---|LED 08|---<---|LED 07|---<---|LED 06|--<-+
+//         +------+       +------+       +------+       +------+       +------+       +------+    |
+//                                                                                                |
+//    +--------------->-------------->-------------->-------------->-------------->---------------+
+//    |
+//    |    +------+       +------+       +------+       +------+       +------+       +------+
+//    +----|LED 05|---<---|LED 04|---<---|LED 03|---<---|LED 02|---<---|LED 01|---<---|LED 0 |--<-- DATA_PIN 
+//         +------+       +------+       +------+       +------+       +------+       +------+
