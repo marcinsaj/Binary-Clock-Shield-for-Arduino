3a79011294ded38250a7f28095889ba83fcda642
Marcin Saj <31963121+marcinsaj@users.noreply.github.com>
2018-11-17 11:04:34 +0100

    Update 02-ColorPalette.ino

--- example/02-ColorPalette/02-ColorPalette.ino
+++ example/02-ColorPalette/02-ColorPalette.ino
@@ -22,10 +22,10 @@
 #include <FastLED.h>            // https://github.com/FastLED/FastLED
 
 #define LED_PIN     A3          // Data pin that LEDs data will be written out over
-#define NUM_LEDS    17          // LEDs using for RGB Calibration
+#define NUM_LEDS    17          // All LEDs on shield 
 #define BRIGHTNESS  60          // The best tested LEDs brightness 20-60
 #define LED_TYPE    WS2812B     // Datasheet: http://bit.ly/LED-WS2812B
-#define COLOR_ORDER GRB         // RGB, GRB, GBR etc.
+#define COLOR_ORDER GRB         // For color ordering use this sketch: http://bit.ly/RGBCalibrate   
 #define UPDATES_PER_SECOND 100  // Updates per second
 
 CRGB leds[NUM_LEDS];            // Define the array of LEDs
@@ -40,13 +40,13 @@ extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 void setup() 
 {
     // Important power-up safety delay
-    delay( 3000 ); 
+    delay(3000); 
 
-    // Limit my draw to 450mA at 5V of power draw
+    // Limit draw to 450mA at 5V of power draw
     FastLED.setMaxPowerInVoltsAndMilliamps(5,450);
   
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(  BRIGHTNESS );
+    FastLED.setBrightness(BRIGHTNESS);
     
     currentPalette = RainbowColors_p;
     currentBlending = LINEARBLEND;
