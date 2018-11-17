f781d936bca13502c71317cc656e5b4f34d960cb
Marcin Saj <31963121+marcinsaj@users.noreply.github.com>
2018-11-17 11:16:51 +0100

    Create 04-PiezoMelody-TheImperialMarch.ino

---
+++ example/04-PiezoMelody/04-PiezoMelody-TheImperialMarch.ino
@@ -0,0 +1,65 @@
+// Binary Clock Shield for Arduino by Marcin Saj https://nixietester.com
+// https://github.com/marcinsaj/Binary-Clock-Shield-for-Arduino
+//
+// Piezo Melody Example
+// Original code you can find here:
+// http://www.arduino.cc/en/Tutorial/Tone
+// 
+// This example shows how to use piezo with the tone() command to generate notes.
+
+#include "pitches.h"          // Need to create the pitches.h library: https://arduino.cc/en/Tutorial/ToneMelody
+
+#define PIEZO       11        // The number of the Piezo pin. Piezo datasheet: http://bit.ly/PIEZO-KLJ1230
+
+// Notes in the melody "The Imperial March":
+const int melody[] PROGMEM = 
+{
+    NOTE_A4,  NOTE_A4,  NOTE_A4,  NOTE_F4,  NOTE_C5,  NOTE_A4,  NOTE_F4,  NOTE_C5,
+    NOTE_A4,  NOTE_E5,  NOTE_E5,  NOTE_E5,  NOTE_F5,  NOTE_C5,  NOTE_GS4, NOTE_F4,
+    NOTE_C5,  NOTE_A4,  NOTE_A5,  NOTE_A4,  NOTE_A4,  NOTE_A5,  NOTE_GS5, NOTE_G5,
+    NOTE_FS5, NOTE_F5,  NOTE_FS5, 0,        NOTE_AS4, NOTE_DS5, NOTE_D5,  NOTE_CS5,
+    NOTE_C5,  NOTE_B4,  NOTE_C5,  0,        NOTE_F4,  NOTE_GS4, NOTE_F4,  NOTE_A4,
+    NOTE_C5,  NOTE_A4,  NOTE_C5,  NOTE_E5,  NOTE_A5,  NOTE_A4,  NOTE_A4,  NOTE_A5,
+    NOTE_GS5, NOTE_G5,  NOTE_FS5, NOTE_F5,  NOTE_FS5, 0,        NOTE_AS4, NOTE_DS5,
+    NOTE_D5,  NOTE_CS5, NOTE_C5,  NOTE_B4,  NOTE_C5,  0,        NOTE_F4,  NOTE_GS4,
+    NOTE_F4,  NOTE_C5,  NOTE_A4,  NOTE_F4,  NOTE_C5,  NOTE_A4,                            
+};
+
+// Note durations: 4 = quarter note, 8 = eighth note, etc.:
+// Some notes durations have been changed (1, 3, 6) to make them sound better
+const byte noteDurations[] PROGMEM = 
+{
+    2, 2, 2, 3, 6, 2, 3, 6, 
+    1, 2, 2, 2, 3, 6, 2, 3, 
+    6, 1, 2, 3, 6, 2, 4, 4, 
+    8, 8, 4, 3, 4, 2, 4, 4,
+    8, 8, 4, 3, 6, 2, 3, 6,
+    2, 3, 6, 1, 2, 3, 8, 2,
+    4, 4, 8, 8, 4, 4, 4, 2,
+    4, 4, 8, 8, 4, 4, 4, 2, 
+    3, 8, 2, 3, 8, 1,                                       
+};
+
+void setup() 
+{
+    // Nothing to setup this time
+}
+
+void loop() 
+{
+    for (int thisNote = 0; thisNote < 70; thisNote++) 
+    {   
+        // To calculate the note duration, take one second divided by the note type.
+        // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        int noteDuration = 1000 / pgm_read_byte(&noteDurations[thisNote]);
+        tone(Piezo, pgm_read_word(&melody[thisNote]), noteDuration);
+
+        // To distinguish the notes, set a minimum time between them.
+        // The note's duration + 30% seems to work well:
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        // Stop the tone playing:
+        noTone(Piezo);
+    }
+    delay(2000); 
+}
